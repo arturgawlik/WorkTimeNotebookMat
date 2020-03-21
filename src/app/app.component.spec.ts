5728f3b360d2b03a1fe9e7755266071190ced3f3
arturgawlik <artur.gawlik@icloud.com>
2020-03-21 14:11:29 +0100

    add some tests to AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -7,8 +7,6 @@ import { FetchingService } from './services/fetching.service';
 
 describe('AppComponent', () => {
 
-  let isLoadingSpy;
-
   beforeEach(async(() => {
 
     // // const fetchingServiceSpy = jasmine.createSpyObj('FetchingService', [], ['isLoading']);
@@ -30,19 +28,13 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('test test', () => {
-    const service = TestBed.get(FetchingService);
-
-    expect(service.isLoading).toBeTruthy();
-  });
-
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it('should have isFetching flag as true', () => {
+  it('should have isFetching flag initialy  as true', () => {
     const fixture = TestBed.createComponent(AppComponent);
     expect(fixture.componentInstance.isFetching).toBeTrue();
   });
@@ -65,12 +57,55 @@ describe('AppComponent', () => {
     
     // then ngAfterViewInit call to hide progress bar
     tick(500);
-
+    
     fixture.detectChanges();
 
     // after that progress bar should disappear
     matProcessBar = fixture.nativeElement.querySelector('mat-progress-bar');
     expect(matProcessBar).toBeFalsy();
+  }));
+
+  it('should show progress-bar when isFetching flag is set as true and hide when isFetching flag is set as false', fakeAsync(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    tick(500);
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('mat-progress-bar')).toBeFalsy(' after full load progress-bar should not be visible');
+
+    fixture.componentInstance.isFetching = true;
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('mat-progress-bar')).toBeTruthy('after set AppComponent.isFetching as true progress-bar should be visible');
+
+    fixture.componentInstance.isFetching = false;
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('mat-progress-bar')).toBeFalsy('after set AppComponent.isFetching as false progress-bar should not be visible');
+
+  }));
+
+  it('should chnage AppComponent.isFetching flag as false after FetchingService.isLoading pushes false', fakeAsync(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    tick(500);
+    fixture.componentInstance.isFetching = true;
+
+    const fetchingService = TestBed.get(FetchingService);
+    fetchingService.hide();
+    tick(500);
+
+    // AppComponent.isFetching should be false after FetchingService.hide() was called
+    expect(fixture.componentInstance.isFetching).toBeFalse();
+  }));
+
+  it('should chnage AppComponent.isFetching flag as true after FetchingService.isLoading pushes true', fakeAsync(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    tick(500);
+
+    const fetchingService = TestBed.get(FetchingService);
+    fetchingService.show();
+    
+    // AppComponent.isFetching should be true after FetchingService.show() was called
+    expect(fixture.componentInstance.isFetching).toBeTrue();
   }));
 
 });
